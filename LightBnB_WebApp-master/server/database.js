b15a557e39440bfdb0b168eeaca942a1ee3053d6
ilaksono <ian.laksono@gmail.com>
2020-10-14 01:59:31 +0000

    refactor pg module

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,22 +1,12 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'lightbnb'
-});
-
-
+const db = require('./db/index');
 /// Users
-
 /**
  * Get a single user from the database given their email.
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  return pool.query(`
+  return db.query(`
   SELECT * FROM users WHERE email LIKE $1;
   `, [email]).then(res => {
     return res.rows[0];
@@ -32,7 +22,7 @@ exports.getUserWithEmail = getUserWithEmail;
  */
 const getUserWithId = function (id) {
 
-  return pool.query(`
+  return db.query(`
   SELECT * FROM users WHERE id=$1;
   `, [id]).then(res => { res.rows[0]; })
     .catch(err => null);
@@ -47,7 +37,7 @@ exports.getUserWithId = getUserWithId;
  */
 const addUser = function (user) {
 
-  return pool.query(`
+  return db.query(`
   INSERT INTO users (name, email, password) 
   VALUES($1, $2, $3)
   RETURNING *;
@@ -66,7 +56,7 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return pool.query(`
+  return db.query(`
   SELECT properties.*, reservations.*, avg(rating) as average_rating
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
@@ -122,7 +112,7 @@ LEFT JOIN property_reviews ON properties.id = property_id
   ORDER BY cost_per_night
     LIMIT $${queryParams.length};`;
 
-  return pool.query(queryString, queryParams).then(res => res.rows);
+  return db.query(queryString, queryParams).then(res => res.rows);
 };
 exports.getAllProperties = getAllProperties;
 
@@ -136,7 +126,7 @@ const addProperty = function (property) {
   const queryParams = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url,
   Number(property.cost_per_night) * 100, property.street, property.city, property.province, property.post_code, property.country,
   property.parking_spaces, Number(property.number_of_bathrooms), Number(property.number_of_bedrooms)];
-  return pool.query(`
+  return db.query(`
   INSERT INTO properties (owner_id, title, description, 
     thumbnail_photo_url,cover_photo_url,
     cost_per_night,
